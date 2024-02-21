68053db88def25a0d95a3f90d500fab77a5d9397
FranciscoGontijo <franciscoacmg@gmail.com>
2024-02-21 16:32:07 -0300

    Adding functionalities to number buttons, refactor how to select every number button

--- index.js
+++ index.js
@@ -4,16 +4,6 @@
 const acButton = document.querySelector('#button-AC');
 const changeOperatorButton = document.querySelector('#button-change-operator');
 const percentButton = document.querySelector('#button-percent');
-const number7Button = document.querySelector('#button-number-7');
-const number8Button = document.querySelector('#button-number-8');
-const number9Button = document.querySelector('#button-number-9');
-const number4Button = document.querySelector('#button-number-4');
-const number5Button = document.querySelector('#button-number-5');
-const number6Button = document.querySelector('#button-number-6');
-const number1Button = document.querySelector('#button-number-1');
-const number2Button = document.querySelector('#button-number-2');
-const number3Button = document.querySelector('#button-number-3');
-const number0Button = document.querySelector('#button-number-0');
 const pointButton = document.querySelector('#button-point');
 const plusButton = document.querySelector('#button-plus');
 const minusButton = document.querySelector('#button-minus');
@@ -21,9 +11,28 @@ const devideButton = document.querySelector('#button-devide');
 const multiplyButton = document.querySelector('#button-multiply');
 const equalButton = document.querySelector('#button-equal');
 
+// Select all number buttons and add functionality to change the viewer text
+const numberButtons = document.querySelectorAll('.number-button');
+numberButtons.forEach((button) => {
+    let buttonValue = button.textContent
+    button.addEventListener('click', () => {
+        creatingViewerText(buttonValue);
+    })
+})
+
 // Viewer text
 const viewer = document.querySelector('.viewer');
 const viewerText = document.createElement('h3');
-viewerText.innerHTML = '4537';
+viewerText.innerHTML = '';
 
 viewer.appendChild(viewerText);
+
+const creatingViewerText = (number) => {
+    let newText = (viewerText.innerHTML.replace(/\s/g, '') + number).replace(/\s/g, '');
+    viewerText.innerHTML = newText;
+    console.log(newText);
+};
+
+
+
+
