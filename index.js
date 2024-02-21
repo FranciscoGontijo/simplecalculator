888829aa75b0715a978570422fcf2e087c391e72
FranciscoGontijo <franciscoacmg@gmail.com>
2024-02-21 14:59:19 -0300

    Select all buttons at js file

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+//Select buttons
+const acButton = document.querySelector('#button-AC');
+const changeOperatorButton = document.querySelector('#button-change-operator');
+const percentButton = document.querySelector('#button-percent');
+const number7Button = document.querySelector('#button-number-7');
+const number8Button = document.querySelector('#button-number-8');
+const number9Button = document.querySelector('#button-number-9');
+const number4Button = document.querySelector('#button-number-4');
+const number5Button = document.querySelector('#button-number-5');
+const number6Button = document.querySelector('#button-number-6');
+const number1Button = document.querySelector('#button-number-1');
+const number2Button = document.querySelector('#button-number-2');
+const number3Button = document.querySelector('#button-number-3');
+const number0Button = document.querySelector('#button-number-0');
+const pointButton = document.querySelector('#button-point');
+const plusButton = document.querySelector('#button-plus');
+const minusButton = document.querySelector('#button-minus');
+const devideButton = document.querySelector('#button-devide');
+const multiplyButton = document.querySelector('#button-multiply');
+const equalButton = document.querySelector('#button-equal');
+
+// Viewer text
+const viewer = document.querySelector('.viewer');
+const viewerText = document.createElement('h3');
+viewerText.innerHTML = '4537';
+
+viewer.appendChild(viewerText);
